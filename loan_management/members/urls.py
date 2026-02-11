306ce17266f797f6275f566251f88ee141f9183d
tulasibhandari <tulasi.bhandari@outlook.com>
2026-02-11 18:27:14 +0545

    updated members/url.py for importing member with excel

--- loan_management/members/urls.py
+++ loan_management/members/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('create/', views.member_create, name='member_create'),
     path('<str:member_number>/', views.member_detail, name='member_detail'),
     path('ajax/search/', views.member_search_ajax, name='member_search_ajax'),
+
+    # Excel operations
+    path('excel/template/', views.download_template, name='download_template'),
+    path('excel/import/', views.download_template, name='import_page'),
+
 ]
